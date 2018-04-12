47be34975614519e6c25d53cc33b387dcc5c1bf9
versustune <Nanni1806>
2018-04-12 12:37:43 +0200

    - changed for to while

--- src/de/versustune/problems/Problem_03.java
+++ src/de/versustune/problems/Problem_03.java
@@ -5,16 +5,18 @@ import de.versustune.Utils;
 public class Problem_03 implements Problems{
 
     public static void execute() {
-        Utils.startText("largest Prime Number started...");
+        Utils.startText("largest Prime Factor started...");
         long number = 600851475143L;
 
-        long highestPrime = -1;
-        for (long i = 2; i <= number; ++i) {
+        long highestPrime = 0; // init
+        long i = 2;
+        while (i <= number) {
             if (number % i == 0) {
                 highestPrime = i;
                 number /= i;
                 --i;
             }
+            ++i;
         }
 
         Utils.sumText(highestPrime);
