96b63dceabfb1ca4865e4a86068828325c9b8b52
versustune <Nanni1806>
2018-04-12 12:47:04 +0200

    - added Problems to Execute

--- src/de/versustune/Main.java
+++ src/de/versustune/Main.java
@@ -7,6 +7,15 @@ public class Main {
     public static void main(String[] args) {
         Utils.setUnderlineFormat("-");
         Utils.startLogo();
+        Problem_01.execute();
+        Problem_02.execute();
         Problem_03.execute();
+        Problem_04.execute();
+        Problem_05.execute();
+        Problem_06.execute();
+        Problem_07.execute();
+        Problem_08.execute();
+        Problem_09.execute();
+        Problem_10.execute();
     }
 }
